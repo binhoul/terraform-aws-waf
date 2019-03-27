d0784dbee7f5f266663e95096750f8a7c0428ef2
libinhou <bhli@thoughtworks.com>
2019-03-27 11:29:14 +0800

    Update readme, comment custom function

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# TODO
+# Terraform AWS WAF
+
+This module is based on [AWS WAF Security Automations](https://aws.amazon.com/cn/solutions/aws-waf-security-automations/) written in cloudformation. It is not so good for deploying cloudformation will take more than 15 min, and we use terraform writing our infrastructure.
+
+## What does this AWS Solution do?
+
+AWS WAF is a web application firewall that enables customers to quickly create custom, application-specific rules that block common attack patterns that can affect application availability, compromise security, or consume excessive resources. AWS WAF can be completely administered via APIs which makes security automation easy, enabling rapid rule propagation and fast incident response.
+
+Configuring a web application firewall strategy can be challenging and burdensome to large and small organizations alike, especially for those who do not have dedicated security teams. To simplify this process, AWS offers a solution that uses AWS CloudFormation to automatically deploy a set of AWS WAF rules designed to filter common web-based attacks. Users can select from preconfigured protective features that define the rules included in an AWS WAF web access control list (web ACL), as depicted in the image to the right. Once the solution is deployed, AWS WAF will begin inspecting web requests to the user’s existing Amazon CloudFront distributions or Application Load Balancers, and block them when applicable.
+
+## AWS Solution overview
+
+The AWS WAF Security Automations solution provides fine-grained control over the requests attempting to access your web application. The diagram below presents the architecture you can build using the solution's implementation guide and accompanying AWS CloudFormation template.
+
+At the core of the design is an AWS WAF web ACL that acts as central inspection and decision point for all incoming requests. The protective functions you choose to activate will determine the custom rules that are added to your web ACL.
+
+![image](./waf-security-automations-architecture.png)
+
+## AWS WAF Security Automations solution architecture
+Honeypot (A): This component creates a honeypot to lure and deflect content scrapers and bad bots. A discrete API Gateway endpoint (embedded in the web application) triggers a custom AWS Lambda function, which intercepts the suspicious request and adds the source IP address to the AWS WAF block list.
+
+**SQL injection (B) and cross-site scripting (C) protection**: The solution automatically configures two native AWS WAF rules that protect against common SQL injection or cross-site scripting (XSS) patterns in the URI, query string, or body of a request.
+
+**Log parsing (D)**: A custom AWS Lambda function automatically parses access logs to identify suspicious behavior and add the corresponding source IP addresses to an AWS WAF block list.
+
+**Manual IP lists (E)**: This component creates two specific AWS WAF rules that allow you to manually insert IP addresses that you want to block (blacklist) or allow (whitelist).
+
+**IP-list parsing (F)**: A custom AWS Lambda function automatically checks third-party IP reputation lists hourly for malicious IP addresses to add to an AWS WAF block list.
+
+**HTTP flood protection (G)**: This component configures a rate-based rule that automatically blocks web requests from a client once they exceed a configurable threshold.
+
+## Deployment
+
+### Assumptions
+
+### Usage example
+```hcl
+module "waf" {
+  source            = "../modules/cloudfront"
+  access_log_bucket = "access_log_bucket_name"
+  aws_region        = "ap-southeast-1"
+}
+```
+
+
+---
+**Referance:**
+[AWS WAF Security Automations](https://aws.amazon.com/cn/solutions/aws-waf-security-automations/)
---
+++ modules/roles.tf
@@ -0,0 +1,355 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    sid = "STSAssumeRole"
+
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    effect = "Allow"
+  }
+}
+
+###############################################################################
+# Custom resource
+###############################################################################
+resource "aws_iam_role" "lambda_role_custom_resource" {
+  name               = "${var.waf_prefix}-LambdaRoleCustomResource"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role.json}"
+}
+
+resource "aws_iam_role_policy" "custom_resource" {
+  name   = "${var.waf_prefix}CustomResource"
+  role   = "${aws_iam_role.lambda_role_custom_resource.id}"
+  policy = "${data.aws_iam_policy_document.custom_resource.json}"
+}
+
+data "aws_iam_policy_document" "custom_resource" {
+  statement {
+    actions = [
+      "waf:GetChangeToken",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "s3:CreateBucket",
+      "s3:GetBucketLocation",
+      "s3:GetBucketNotification",
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:PutBucketNotification",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "arn:aws:s3:::${var.access_log_bucket}",
+    ]
+  }
+
+  statement {
+    actions = [
+      "lambda:*",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "arn:aws:lambda:${var.aws_region}:${data.aws_caller_identity.current.account_id}:function:*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "waf:GetWebACL",
+      "waf:UpdateWebACL",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      #"arn:aws:waf::${data.aws_caller_identity.current.account_id}:webacl/${aws_waf_web_acl.WAFWebACL.id}"
+      "arn:aws:waf::${data.aws_caller_identity.current.account_id}:webacl/*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "waf:GetRule",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "arn:aws:waf::${data.aws_caller_identity.current.account_id}:rule/*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "waf:CreateRateBasedRule",
+      "waf:GetRateBasedRule",
+      "waf:ListRateBasedRules",
+      "waf:UpdateRateBasedRule",
+      "waf:DeleteRateBasedRule",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "arn:aws:waf::${data.aws_caller_identity.current.account_id}:ratebasedrule/*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "arn:aws:logs:*:*:*",
+    ]
+  }
+}
+
+###############################################################################
+# Bad Bot
+###############################################################################
+resource "aws_iam_role" "lambda_role_bad_bot" {
+  name               = "${var.waf_prefix}-LambdaRoleBadBot"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role.json}"
+}
+
+resource "aws_iam_role_policy" "bad_bot" {
+  name   = "${var.waf_prefix}BadBot"
+  role   = "${aws_iam_role.lambda_role_bad_bot.id}"
+  policy = "${data.aws_iam_policy_document.bad_bot.json}"
+}
+
+data "aws_iam_policy_document" "bad_bot" {
+  statement {
+    actions = [
+      "waf:GetChangeToken",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "waf:GetIPSet",
+      "waf:UpdateIPSet",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "arn:aws:waf::${data.aws_caller_identity.current.account_id}:ipset/${aws_waf_ipset.waf_bad_bot_set.id}",
+    ]
+  }
+
+  statement {
+    actions = [
+      "cloudwatch:GetMetricStatistics",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "arn:aws:logs:*:*:*",
+    ]
+  }
+}
+
+###############################################################################
+# Log Parser
+###############################################################################
+resource "aws_iam_role" "lambda_role_log_parser" {
+  name               = "${var.waf_prefix}-LambdaRoleLogParser"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role.json}"
+}
+
+resource "aws_iam_role_policy" "log_parser" {
+  name   = "${var.waf_prefix}LogParser"
+  role   = "${aws_iam_role.lambda_role_log_parser.id}"
+  policy = "${data.aws_iam_policy_document.log_parser.json}"
+}
+
+data "aws_iam_policy_document" "log_parser" {
+  statement {
+    actions = [
+      "waf:GetChangeToken",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "waf:GetIPSet",
+      "waf:UpdateIPSet",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "arn:aws:waf::${data.aws_caller_identity.current.account_id}:ipset/${aws_waf_ipset.waf_blacklist_set.id}",
+      "arn:aws:waf::${data.aws_caller_identity.current.account_id}:ipset/${aws_waf_ipset.waf_scanner_probe_set.id}",
+    ]
+  }
+
+  statement {
+    actions = [
+      "s3:GetObject",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "arn:aws:s3:::${var.access_log_bucket}/*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "s3:PutObject",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "arn:aws:s3:::${var.access_log_bucket}/aws-waf-security-automations-current-blocked-ips.json",
+    ]
+  }
+
+  statement {
+    actions = [
+      "cloudwatch:GetMetricStatistics",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "arn:aws:logs:*:*:*",
+    ]
+  }
+}
+
+###############################################################################
+# Reputation List Parser
+###############################################################################
+resource "aws_iam_role" "lambda_role_reputation_list_parser" {
+  name               = "${var.waf_prefix}-LambdaRoleReputationListParser"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role.json}"
+}
+
+resource "aws_iam_role_policy" "reputation_list_parser" {
+  name   = "${var.waf_prefix}ReputationListParser"
+  role   = "${aws_iam_role.lambda_role_reputation_list_parser.id}"
+  policy = "${data.aws_iam_policy_document.reputation_list_parser.json}"
+}
+
+data "aws_iam_policy_document" "reputation_list_parser" {
+  statement {
+    actions = [
+      "waf:GetChangeToken",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "waf:GetIPSet",
+      "waf:UpdateIPSet",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "arn:aws:waf::${data.aws_caller_identity.current.account_id}:ipset/${aws_waf_ipset.waf_reputation_set.id}",
+    ]
+  }
+
+  statement {
+    actions = [
+      "cloudwatch:GetMetricStatistics",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      "arn:aws:logs:*:*:*",
+    ]
+  }
+}
